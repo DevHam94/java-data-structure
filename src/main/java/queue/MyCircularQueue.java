f6f0e7c2a6a231f76986343b6a98c120af9e0da4
admin <gkaeogus7@gmail.com>
2023-09-25 18:29:32 +0900

    230925

---
+++ src/main/java/queue/MyCircularQueue.java
@@ -0,0 +1,43 @@
+package main.java.queue;
+
+public class MyCircularQueue<T> implements IQueue<T> {
+
+    private T[] elements;
+    private int front;
+    private int rear;
+    private int maxSize;
+
+    public MyCircularQueue(int size) {
+        this.elem
+    }
+
+    @Override
+    public void offer(T data) {
+
+    }
+
+    @Override
+    public T poll() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
